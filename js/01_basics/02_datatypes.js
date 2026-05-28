a0687688df3c304a86127eeb4eaa04e68d716f1b
RamK2006 <itsyash23108@gmail.com>
2026-05-28 14:37:50 +0530

    datatypes basic

---
+++ js/01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treats all js code as newer version
+
+// alert() -> won't work on node js but will on browser
+
+console.log(3
+    +    
+    3); // hell of code readability
+
+let name = 'hitesh'
+let age = 9
+let isLoggedIn = false
+
+// number, bigint ( large numbers)
+// string
+// boolean
+//null -> standalone value ( khali)
+// undefined -> value not defined 
+//symbol => unique 
+// these were primitive
+
+//object 
+
+console.log(typeof(name)); // string
+console.log(typeof(null)); // object type
+console.log(typeof(undefined)); // undefined itself
+
+
