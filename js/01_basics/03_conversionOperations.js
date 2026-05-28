25e1d4153ac23808db06d8f69c2a7c8afc2867e2
RamK2006 <itsyash23108@gmail.com>
2026-05-28 15:04:23 +0530

    nothing but small additions

--- js/01_basics/03_conversionOperations.js
+++ js/01_basics/03_conversionOperations.js
@@ -24,9 +24,40 @@ console.log(score); // null
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn); // for every non zero value -> true, false at 0 and "" empty string
+console.log(booleanIsLoggedIn); // for every non zero value -> true
+//  false at 0 and "" empty string
 
 
 let nber = 33
 let stringnber = String(nber)
 console.log(typeof stringnber);
+
+// ************* Operations =>
+
+let value =3
+let negvalue = -value
+console.log(negvalue); //-3
+
+let str1 = 'Radhe '
+let str2 = 'Radhe'
+let str3 = str1 + str2
+console.log(str3)
+
+// where the problem occurs 
+console.log("1"+2); // 12
+console.log(1+"2"+2+4); // 1224 , means add ni krega na
+console.log(1+2+3+"3"); // 63, but ab idhr to isne add krdiya, problem h
+
+console.log( (3+4)*5%9); // ese code ni likhte, likhte h to hire ni hote, to ni likhte
+
+// special conversions
+
+console.log(+true); // true nahi 1 dega ans
+//Ye unary plus operator hai because iske sirf ek operand hai.
+
+//console.log(true+); // error dega ye kyuki right m value nhi ,binary operator addition hai
+
+console.log(+"") // 0 dega ans 
+
+// JavaScript mein unary + operator type conversion karta hai.
+// Ye value ko Number mein convert karne ki koshish karta hai.
