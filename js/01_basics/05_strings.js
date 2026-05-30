c2093160049364b5fd204d2fa8b85fb7c6e3f8a1
RamK2006 <itsyash23108@gmail.com>
2026-05-30 07:49:29 +0530

    added strings

---
+++ js/01_basics/05_strings.js
@@ -0,0 +1,21 @@
+const name = "hello bhai"
+const repocnt = 50
+
+console.log( name + repocnt + " Value" )// this is not a good way to write codes or add strings or data 
+// not at all preferred 
+// do this instead
+
+console.log(`Hello my name is ${name} and my repo count is ${repocnt}`);
+
+const gameName = new String('gameboykagame')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf(k));
+
+
+
+
